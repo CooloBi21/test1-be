e34b9600c065ae455938edee8119954b1dbe98f1
CooloBi21 <lenguyenhoang21hs@gmail.com>
2026-09-07 14:35:13 +0700

    chore: add cookie parser middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   // 1. Bật CORS (Lấy danh sách origin được phép bao gồm cả Local và Production Vercel)
   const allowedOrigins = [
